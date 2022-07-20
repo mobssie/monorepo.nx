8fa6ef552cb1548204a64de02d2c4fea2e2a0ee0
Juri Jung <mobssie@wemakeprice.com>
2022-07-20 17:58:01 +0900

    express 기본 설정

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,11 +3,37 @@ import { Message } from '@mobssie-nx/api-interfaces';
 
 const app = express();
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 const greeting: Message = { message: 'Welcome to api!' };
+
+
+let id = 2;
+const todoList = [{
+  id: 1,
+  text: '할일 1',
+  done: false,
+}];
+
 
 app.get('/api', (req, res) => {
   res.send(greeting);
 });
+
+app.get('/api/todo', (req, res)=> {
+  res.json(todoList);
+})
+
+app.post('/api/todo', (req, res)=> {
+  const { text, done } = req.body;
+  todoList.push({
+    id: id++,
+    text,
+    done,
+  });
+  return res.send('success'); 
+})
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
